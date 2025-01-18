9c8f2ddd31068ae65fe24ef5e24043cbf3347c44
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2025-01-18 14:52:15 -0300

    feat: Criação dos dados da api

---
+++ src/dados.js
@@ -0,0 +1,30 @@
+export const pets = [
+  {
+    id: "160c8dfd-ecb3-4df3-9778-5c470fd69c94",
+    nome: "Amora",
+    raca: "Akita",
+    idade: 3,
+    nome_tutor: "Rafael",
+  },
+  {
+    id: "961c0464-f6b7-415a-a5b5-9451215b4d2a",
+    nome: "Eva",
+    raca: "Shitzu",
+    idade: 3,
+    nome_tutor: "Josiane",
+  },
+  {
+    id: "5636850b-8278-4ef0-aed9-8fb94fe0a540",
+    nome: "Thor",
+    raca: "SRD",
+    idade: 2,
+    nome_tutor: "Sonia",
+  },
+  {
+    id: "77d20071-1675-46aa-b3d3-3e47f0fbd4a4",
+    nome: "Luna",
+    raca: "Shitzu",
+    idade: 2,
+    nome_tutor: "Benicio",
+  },
+];
